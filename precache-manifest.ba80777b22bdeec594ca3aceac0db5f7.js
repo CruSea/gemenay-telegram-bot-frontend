ffc24700f5dc035406d38422b7114f7de09ef323
birukbelay <belay.bura@gmail.com>
2020-10-19 15:07:13 +0300

    Updates

---
+++ precache-manifest.ba80777b22bdeec594ca3aceac0db5f7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cacf040b7e3826eae34e07d76aa961",
+    "url": "/GemenayeAdmin/index.html"
+  },
+  {
+    "revision": "ba9ea5669cc23c526c54",
+    "url": "/GemenayeAdmin/static/css/3.a8f4dade.chunk.css"
+  },
+  {
+    "revision": "9d3d8f35074907a092c5",
+    "url": "/GemenayeAdmin/static/css/main.6b2e0bdb.chunk.css"
+  },
+  {
+    "revision": "020edd2d0b4f0e0f32b3",
+    "url": "/GemenayeAdmin/static/js/2.ac9a641a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/GemenayeAdmin/static/js/2.ac9a641a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9ea5669cc23c526c54",
+    "url": "/GemenayeAdmin/static/js/3.5ecafa5a.chunk.js"
+  },
+  {
+    "revision": "10f7d8adbc150eaf7cb6",
+    "url": "/GemenayeAdmin/static/js/4.8075c9f0.chunk.js"
+  },
+  {
+    "revision": "9d3d8f35074907a092c5",
+    "url": "/GemenayeAdmin/static/js/main.6d71fe88.chunk.js"
+  },
+  {
+    "revision": "2c78298fc217ee225b71",
+    "url": "/GemenayeAdmin/static/js/runtime-main.1448b450.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/GemenayeAdmin/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/GemenayeAdmin/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/GemenayeAdmin/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/GemenayeAdmin/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/GemenayeAdmin/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/GemenayeAdmin/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/GemenayeAdmin/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/GemenayeAdmin/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/GemenayeAdmin/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/GemenayeAdmin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/GemenayeAdmin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/GemenayeAdmin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/GemenayeAdmin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/GemenayeAdmin/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
